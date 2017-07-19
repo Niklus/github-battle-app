104cb549b46016ed0314dc7349c5276eb1e55308
niklus <niklasoti@gmail.com>
2017-07-19 16:24:12 +0200

    update

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ var Link = require('react-router-dom').Link;
 function Home () {
     return (
       <div className='home-container'>
-        <h1>Github Battle: Battle your friends... and stuff.</h1>
+        <h1>Github Battle: Battle your friends.</h1>
         <Link className='button' to='/battle'>Battle</Link>
       </div>
     )
